72271742dddee90a3e7d2155d06ca52677b03800
Druggistweb <146789857+Druggistweb@users.noreply.github.com>
2023-10-03 03:35:38 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxPpm_VGtat81n-NgLDsR-uK8Kdxc-_MSXPFjsNHz5xs598-plQ7z5U_VtI-IHWLewq/exec'
+
+const form = document.forms['form']
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  .then(response => alert("Thank you! your form is submitted successfully." ))
+  .then(() => { window.location.reload(); })
+  .catch(error => console.error('Error!', error.message))
+})

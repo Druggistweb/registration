4eec026c7ecfb30bb27f9b7efac2d296e32684ae
Druggistweb <146789857+Druggistweb@users.noreply.github.com>
2023-10-14 13:42:27 +0300

    Update main.js

--- main.js
+++ main.js
@@ -5,14 +5,8 @@ const form = document.forms['form']
 form.addEventListener('submit', e => {
   e.preventDefault()
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  .then(document.getElementById("load").style.visibility = `visible`))
   .then(response => alert("Thank you! your form is submitted successfully." ))
   .then(() => { window.location.reload(); })
   .catch(error => console.error('Error!', error.message))
 })
-function loading(){
-  document.getElementById("load").style.visibility = `visible`;
-}
-function stop(){
-  document.getElementById("load").style.visibility = `hidden`;
-}
-
